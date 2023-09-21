ff9d1ffe8a1302587d54097ea72a34a8f9f7725b
Gianlorenzo Urbano <gianlorenzourbano@gmail.com>
2023-09-21 20:40:52 +0200

    SQ-7 test commit

--- SquealerApp/src/App.tsx
+++ SquealerApp/src/App.tsx
@@ -1,23 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { motion } from "framer-motion";
-
-const Loading = () => {
-  return <div className="loading">Loading...</div>;
-};
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log("App mounted");
-    fetch("/api/account")
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  }, []);
 
   return (
 	<>
