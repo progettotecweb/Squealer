5dce492adeeb301c33fc2547cb1b8239adc32318
Gianlorenzo Urbano <gianlorenzourbano@gmail.com>
2023-09-22 02:43:11 +0200

    SQ-4 adds basic routing

--- SquealerApp/src/App.tsx
+++ SquealerApp/src/App.tsx
@@ -1,9 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function App() {
 
   return (
 	<>
 		<h1>Homepage!</h1>
 		<p>This is just a test to see if i correctly understand branches</p>
+		<Link to="/">Home</Link>
 	</>
   );
 }
--- SquealerApp/src/main.tsx
+++ SquealerApp/src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+// Squealer App routes (base is /Home)
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+
+], {basename: '/Home'})
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
