14be588ef4212276df89b241aabae329c4d3992a
folli <simone.folli2@studio.unibo.it>
2023-09-28 13:19:39 +0200

    icona nuova e import bootstrap

---
+++ SquealerSMMDashboard/src/components/SMM.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+
+defineProps<{ msg: string }>()
+
+
+</script>
+
+<template >
+   <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+    </p>
+    </div>
+</template>
+
+<style scoped></style>
