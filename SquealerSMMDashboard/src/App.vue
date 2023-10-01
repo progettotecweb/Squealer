b51054af9adcf72fcfd26b862b224fdc4ed5a858
folli <simone.folli2@studio.unibo.it>
2023-10-01 14:30:48 +0200

    modificata App.vue

--- SquealerSMMDashboard/src/App.vue
+++ SquealerSMMDashboard/src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
-import SMM from './components/SMM.vue'
+import SMM_main from './components/SMM_main.vue'
 
 </script>
 
 <template >
-  <SMM msg="cose" />
+  <header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+    <a class="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="/">Home</a>
+  </header>
+  <div class="container-fluid ">
+    <div class="row flex-xl-nowrap">
+      <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
+        colonnna sinistra 
+      </div>
+      <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content bg-secondary flex flex-col justify-center items-center text-center text-w undefined text-slate-50" role="main">
+        <h1 class="bd-title" id="content">Dashboard</h1>
+        <div>
+          <SMM_main />
+        </div>
+        fine main
+      </main>
+    </div>
+  </div>
+  
 </template>
 
 <style >
---
+++ SquealerSMMDashboard/src/components/SMM_main.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+// const count = ref(0)
+
+
+let id = 0
+const newAccount = ref('')
+const followed_accounts = ref([{id: id++, text: 'ciao'}])
+
+function addAccount() {
+    followed_accounts.value.push({ id: id++ , text: newAccount.value})
+    newAccount.value = ''
+}
+
+function removeAccount(id: {}){
+    followed_accounts.value = followed_accounts.value.filter( (account) => account !== id)
+}
+
+
+</script>
+
+<template>
+    <div >
+        <div>
+            <form @submit.prevent="addAccount">
+                <input v-model="newAccount">
+                <button>Add account</button>    
+            </form>
+        </div>
+        <div v-for="account in followed_accounts" :key="account.id">
+            <div class="card bg-success h-25 w-25">
+                {{ account.text }}
+                <button class="exit-button" @click="removeAccount(account)">X</button>
+            </div> 
+
+        </div>
+    </div>
+</template>
+
+<style ></style>
