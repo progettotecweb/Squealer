a0981294c792246a3d06043d01b9cb9506b9edbb
Enrico <enricofen300@gmail.com>
2023-11-03 18:14:25 +0100

    boxcontent frontend 0.0.1

--- public/js/moderator.js
+++ public/js/moderator.js
@@ -60,3 +60,30 @@ async function getUserData() {
 
 getUserData();
 
+window.onload = function () {
+    const userSection = document.getElementById("userSection");
+    const channelSection = document.getElementById("channelSection");
+    const squealSection = document.getElementById("squealSection");
+
+    userSection.addEventListener("click", (e) => {
+        changeSectionClass(e.target);
+    });
+
+    channelSection.addEventListener("click", (e) => {
+        changeSectionClass(e.target);
+
+    });
+
+    squealSection.addEventListener("click", (e) => {
+        changeSectionClass(e.target);
+    });
+}
+
+function changeSectionClass(newSection) {
+    //before adding the new class i remove the old one
+    const activeSection = document.querySelector(".active-section");
+    activeSection.classList.remove("active-section");
+
+    //update the section
+    newSection.classList.add("active-section");
+}
