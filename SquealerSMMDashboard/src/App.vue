f229acd6c8b00a54a6f4847fae74c9aedb08d478
folli <simone.folli2@studio.unibo.it>
2023-11-29 14:11:26 +0100

    Refactor UI layout and add user profile component

--- SquealerSMMDashboard/src/App.vue
+++ SquealerSMMDashboard/src/App.vue
@@ -2,6 +2,7 @@
 import {  ref } from 'vue'
 import user_box from './components/user_box.vue'
 import { getUserData } from './components/fetch.ts'
+import user_profile from './components/user_profile.vue';
 
 
 const user = ref<any>(null); 
@@ -20,13 +21,13 @@ user.value.then((data: any) => {
 
 <template >
   <header class="navbar navbar-expand navbar-dark sticky-top">
-    <div class="container-fluid row justify-content-between"><!--mid or more navbar-->
-      <h4 class="col navbar-brand">SMM Dashboard</h4>
-      <div class="col logo">
+    <div class="d-flex justify-content-between w-100">
+      <h4 class="navbar-brand">SMM Dashboard</h4>
+      <div class="logo">
         <img src="/public/squealer.png" alt="squealer-logo" class="img-fluid logo" />
       </div>
-      <div class="col d-none d-lg-block" id="n12345">
-        <a class="btn" href="">Bottone</a>
+      <div class="d-none d-lg-block">
+        <a class="AppBtn" href="">Bottone</a>
       </div>
     </div>
 
@@ -37,7 +38,7 @@ user.value.then((data: any) => {
       <div class="col-12 col-md-3 col-xl-2 sidebar d-none d-lg-block">
         <img :src="`data:${user.img.mimetype};base64,${user.img.blob}`" alt="user-img" style="width: 120px; height: 120px;"/> 
         <p>@{{ user.name }}</p>
-        <a class="btn" href="/"><h4>Home</h4></a>
+        <a class="AppBtn" href="/"><h4>Home</h4></a>
       </div>
 
       <main 
@@ -46,11 +47,15 @@ user.value.then((data: any) => {
       role="main">
         <h1>Dashboard</h1>
         <div>
-          <div class="d-flex justify-content-around flex-wrap " >
-              <user_box 
+          <div class="d-flex justify-content-around flex-wrap " > <!-- si vede se variabile x True-->
+              <user_box
                   v-for="account in controlled_user"  
                   :id="account"></user_box>
           </div>
+          <div><!--componente con info degli account, si vede se variabile x False -->
+            <user_profile></user_profile>
+          </div>
+
         </div>
       </main>
 
@@ -74,13 +79,12 @@ header{
   width: 30pt;
   height: 30pt;
 }
-#n12345{
+.AppBtn{
   background-color: #374e64;
   color: aliceblue;
   border: 1px solid aliceblue;
   border-radius: 5px;
   padding: 5px;
-  width: 40px;
 }
 
 </style>      
