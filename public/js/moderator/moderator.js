62f829f11d52851eb7cf4e6771a422f5bd63c0f0
Enrico <enricofen300@gmail.com>
2024-01-27 15:12:45 +0100

    Update index.html and moderator.css

--- public/js/moderator/moderator.js
+++ public/js/moderator/moderator.js
@@ -1605,18 +1605,18 @@ function addSquealCard(squeal, recipients, div, del = false, viewMore = false) {
     content += '</div>';
 
     let footer = '<div class="squeal-footer">';
-    let reactions = '<div class="squeal-reactions flex-row-reverse d-flex justify-content-around">'
+    let reactions = '<div class="mb-1 mt-1 squeal-reactions d-flex justify-content-around">'
         + '<div>😡 ' + squeal.reactions.m2 + '</div>'
         + '<div>😒 ' + squeal.reactions.m1 + '</div>'
         + '<div>😄 ' + squeal.reactions.p1 + '</div>'
         + '<div>😝 ' + squeal.reactions.p2 + '</div>'
         + '</span>'
         + '</div>';
-    let CM = '<div class="w-33 squeal-cm">'
-        + '<span><b>CM</b>: ' + squeal.cm.label + '</span>'
+    let CM = '<div class="w-33 text-capitalize squeal-cm">'
+        + '<span><i>' + squeal.cm.label + '</i></span>'
         + '</div>';
-    let impressions = '<div w-33 class="squeal-impressions">'
-        + '<span>' + squeal.impressions + ' impression(s)</span>'
+    let impressions = '<div  class="w-33 squeal-impressions text-end">'
+        + '<span><i>' + squeal.impressions + '</i>' + ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"/></svg>` + '</span>'
         + '</div>';
     let controversial = '<div class="squeal-controversial squeal-footer-br">'
         + '<span><i>' + (squeal.controversial === 'true' ? 'Controversial' : 'Not controversial') + '</i></span>'
